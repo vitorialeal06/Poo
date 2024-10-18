c9c7edef9f705b1ad9a637a723f7c161980278e4
VitoriaLeal <115575302+vitorialeal06@users.noreply.github.com>
2024-10-18 17:53:13 -0300

    5

---
+++ Quadrado.java
@@ -0,0 +1,11 @@
+public class Quadrado extends Retangulo{
+
+    Quadrado(double lado1, String cor){
+        super(lado1);
+        this.setLado1(lado1);
+        super.setCor(cor);
+    }
+    public double area(){
+        return this.getLado1() * this.getLado1();
+    }
+}
